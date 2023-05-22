d0f746c0fc57e68f121c738d1f1742cbaca00f85
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-22 16:10:41 -0300

    Aplle and Oranges

--- tdd-by-example2/src/test/java/guru/springframework/MoneyTest.java
+++ tdd-by-example2/src/test/java/guru/springframework/MoneyTest.java
@@ -7,6 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
+    void testMultiplicationDollar() {
+        Dollar five = new Dollar(5);
+
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
+
+        product = five.times(3);
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void TestEqualityDollar() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+
+        assertNotEquals(new Dollar(5), new Franc(5));
+    }
+
+    @Test
     void testMultiplicationFranc() {
         Franc five = new Franc(5);
 
