f8e6c10bc3fefaa6479dd7235dd37efeb55f1e98
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-23 16:14:13 -0300

    @BeforeEach

--- intro-junit5/src/test/java/guru/springframework/GreetingTest.java
+++ intro-junit5/src/test/java/guru/springframework/GreetingTest.java
@@ -1,19 +1,29 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+    private Greeting greeting;
+
+    // Todos com esta annotation vão rodar antes de CADA UM dos testes, se tivermos 2 testes
+    // Este método setUp irá ser executado antes de cada um, ou seja, 2x
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
+        greeting = new Greeting();
         System.out.println(greeting.helloWorld());
 
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting = new Greeting();
+        greeting = new Greeting();
         System.out.println(greeting.helloWorld("John"));
     }
 }
