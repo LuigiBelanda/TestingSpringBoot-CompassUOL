0acdc17d70d1a843b44235533efa7194f8f511ed
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-23 16:23:56 -0300

    JUnit 5 life cycle

--- intro-junit5/src/test/java/guru/springframework/GreetingTest.java
+++ intro-junit5/src/test/java/guru/springframework/GreetingTest.java
@@ -1,10 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
     private Greeting greeting;
+
+    // Todos com esta annotation vão rodar antes de tudo, é o primeiro método a ser executado
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called Onde!!!");
+    }
 
     // Todos com esta annotation vão rodar antes de CADA UM dos testes, se tivermos 2 testes
     // Este método setUp irá ser executado antes de cada um, ou seja, 2x
@@ -16,14 +21,25 @@ class GreetingTest {
 
     @Test
     void helloWorld() {
-        greeting = new Greeting();
         System.out.println(greeting.helloWorld());
 
     }
 
     @Test
     void testHelloWorld() {
-        greeting = new Greeting();
         System.out.println(greeting.helloWorld("John"));
+    }
+
+    // Todos com esta annotation vão rodar DEPOIS de CADA UM dos testes, se tivermos 2 testes
+    // Este método setUp irá ser executado DEPOIS de cada um, ou seja, 2x
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    // Todos com esta annotation vão depois de tudo, é o primeiro método a ser executado
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called Onde!!!");
     }
 }
