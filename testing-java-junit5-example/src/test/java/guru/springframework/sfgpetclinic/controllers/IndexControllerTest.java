88f57fb35b02db56a510df45ebf76729dc8ce612
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-24 13:52:39 -0300

    Assumptions

--- testing-java-junit5-example/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5-example/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -55,5 +56,15 @@ class IndexControllerTest {
 
             System.out.println("I got here 111111111");
         });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionTrueAssumptionIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
     }
 }
