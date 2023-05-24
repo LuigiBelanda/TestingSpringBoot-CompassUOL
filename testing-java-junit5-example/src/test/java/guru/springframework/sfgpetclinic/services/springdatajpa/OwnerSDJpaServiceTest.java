b0ef7cbb6c27704476cc09069fe543bde23e4a3b
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-24 13:17:12 -0300

    Disable tests

---
+++ testing-java-junit5-example/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled(value = "Disabled until we learn Mocking")
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    // Com esta annotation podemos desabilitar este teste
+    // ou até mesmo esta classe inteira de testes (OwnerSDJpaServiceTest)
+    @Disabled
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
