881aac4ea440745b9fd9609335ec2f6577ec6be2
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-25 13:35:08 -0300

    Running Tests - CI - Mock

---
+++ testing-junit5-mockito/src/main/java/guru/springframework/sfgpetclinic/InlineMockTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.Map;
+
+public class InlineMockTest {
+    @Test
+    void testInlineMock() {
+        Map mapMock = mock(Map.class);
+
+
+    }
+}
